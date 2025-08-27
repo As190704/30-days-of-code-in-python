b40d417518c4a833af474484c8e91027ae6972f8
Ashutosh Jha <133184716+As190704@users.noreply.github.com>
2025-08-27 12:28:25 +0000

    Initial commit

---
+++ 3rd day/solutions.py
@@ -0,0 +1,12 @@
+# Q1 >> Using + with spaces
+result = 'Thirty' + ' ' + 'Days' + ' ' + 'Of' + ' ' + 'Python'
+print(result)  # Output: Thirty Days Of Python
+
+# Or using join (cleaner way)
+result = ' '.join(['Thirty', 'Days', 'Of', 'Python'])
+print(result)  # Output: Thirty Days Of Python
+
+#Q2 solutions
+result = 'Coding' + ' ' + 'For' + ' ' + 'All'
+print(result)  # Output: Coding For All
+
