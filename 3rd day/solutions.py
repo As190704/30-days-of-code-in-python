29184bf87b646d15072d6415b9de2d83eca7b727
Ashutosh Jha <133184716+As190704@users.noreply.github.com>
2025-08-27 12:43:09 +0000

    Initial commit

--- 3rd day/solutions.py
+++ 3rd day/solutions.py
@@ -10,6 +10,15 @@ print(result)  # Output: Thirty Days Of Python
 result = 'Coding' + ' ' + 'For' + ' ' + 'All'
 print(result)  # Output: Coding For All
 
-#Q3 solutions
+#Q3 and 4 solutions
 company = "Coding For All"
 print(company)  # Output: Coding For All
+
+#Q5 solutions
+print(len(company)) #Output: 13
+
+#Q6 solutions
+print(company.upper()) 
+
+#Q7 solutions
+print(company.lower())
