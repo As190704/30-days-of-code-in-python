48d7e0905cb072c949c13470917b4d8aaeef7a52
Ashutosh Jha <133184716+As190704@users.noreply.github.com>
2025-10-15 18:31:40 +0000

    initial commit

--- 5th-day/tuple.py
+++ 5th-day/tuple.py
@@ -1,2 +1,10 @@
 empty_tuple =()
 # also empty_tuple = tuple()
+
+#Q2 Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+siblings =('Anil', 'Sumit','Sahil')
+
+#Q3 Join brothers and sisters tuples and assign it to siblings
+brothers =('Anil', 'Sumit','Sahil')
+sisters=('Anila', 'Sunita','Sahila')
+siblings= brothers+sisters
