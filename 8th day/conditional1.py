6a5c79113255312e0f04a20baaeb218229b906d7
Ashutosh Jha <133184716+As190704@users.noreply.github.com>
2025-11-28 16:32:32 +0000

    Intial commit

--- 8th day/conditional1.py
+++ 8th day/conditional1.py
@@ -6,3 +6,26 @@ if age>= 18:
 else :
     missingyears = 18 -age
     print (f"please wait {missingyears} more to drive.")
+
+#q2Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. Output:
+
+age =  int(input("Enter your age"))
+my_age = 21
+
+if my_age > age 
+   diff =my_age-age
+   if diff==1:
+    print("you are one year older .")
+   else:
+    print(f"you are {diff} years older.")
+
+elif age > my_age
+    diff = age -my_age
+    if diff == 1:
+        print ("I am one year older.")
+    else:
+        print(f"you are {diff} years younger old.")
+
+else:
+    print("You are as old as me !\n")
+    print("What are you doing.")
