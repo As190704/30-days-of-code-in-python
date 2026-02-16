88bf7c6ebb72145febe50dec95603f6d2eb59677
Ashutosh Jha <133184716+As190704@users.noreply.github.com>
2026-02-16 19:16:50 +0000

    Commiting new files

--- 2nd day of python/operator 2.py
+++ 2nd day of python/operator 2.py
@@ -8,3 +8,34 @@ age=21
 height=175.65 cm 
 
 #q3
+x = 3+4j
+
+x=complex(3,4)
+
+#q4 defined
+
+
+base=20
+height = 10
+
+Area = 1/2*(base*height)
+print (Area)
+#q4 user
+base= int(input("Enter the base :"))
+height =int(input("Enter the height:"))
+
+Area= 1/2*(base*height)
+print (Area)
+
+#q5
+
+a=5
+b=4
+c=3
+
+perimeter = a+b+c 
+
+print(f"The perimeter is " ,perimeter)
+
+
+#q6
